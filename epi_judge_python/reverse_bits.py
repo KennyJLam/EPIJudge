aa1d11cc8f67e20c37b46f6f8c80beb6a161761e
Kenny Lam <lam.kennyj@gmail.com>
2021-07-10 18:30:00 -0400

    Implement reverse_bits solution

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
 
+swap_cache = [0b0000, 0b1000, 0b0100, 0b1100,
+              0b0010, 0b1010, 0b0110, 0b1110,
+              0b0001, 0b1001, 0b0101, 0b1101,
+              0b0011, 0b1011, 0b0111, 0b1111]
+
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    ret_val = 0
+    mask = 0b1111
+    for i in range(16):
+        ret_val <<= 4
+        ret_val |= swap_cache[x & mask]
+        x >>= 4
+    return ret_val
 
 
 if __name__ == '__main__':
