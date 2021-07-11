36e2e98eb35cfbb67a9638ef7d3b96bd11558fd7
Kenny Lam <lam.kennyj@gmail.com>
2021-07-11 13:37:56 -0400

    Implement primitive multiply solution

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
 
+def add(x: int, y: int) -> int:
+    ret_sum = 0
+    carry_bit = 0
+    add_mag = 1
+    while x != 0 or y != 0 or carry_bit == 1:
+        next_bit = (x & 1) ^ (y & 1) ^ carry_bit
+        if next_bit == 1:
+            ret_sum |= add_mag
+        carry_bit = ((x & 1) & (y & 1)) | ((x & 1) & carry_bit) | ((y & 1) & carry_bit)
+        x >>= 1
+        y >>= 1
+        add_mag <<= 1
+    return ret_sum
+
+
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    ret_prod = 0
+    while y != 0:
+        if y & 1 == 1:
+            ret_prod = add(x, ret_prod)
+        x <<= 1
+        y >>= 1
+    return ret_prod
 
 
 if __name__ == '__main__':
