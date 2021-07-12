409df012d894e86939b775a4bc5ca83d46fa4b1c
Kenny Lam <lam.kennyj@gmail.com>
2021-07-12 17:07:57 -0400

    Implement power x y solution.

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    ret_val = 1
+    pos_y = abs(y)
+    while pos_y != 0:
+        pow_bit = pos_y & ~(pos_y - 1) # get first set bit
+        pos_y &= pos_y - 1 # clear first set bit
+        x_pow = x
+        pow_bit >>= 1
+        while pow_bit != 0:
+            x_pow *= x_pow
+            pow_bit >>= 1
+        ret_val *= x_pow
+    if y < 0:
+        ret_val = 1 / ret_val
+    return ret_val
 
 
 if __name__ == '__main__':
