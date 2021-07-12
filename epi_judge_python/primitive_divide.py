b7ffdb6162401ff541f7138b70828d383dc0b0a8
Kenny Lam <lam.kennyj@gmail.com>
2021-07-12 16:41:17 -0400

    Implement primivite divide solution

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    quotient = 0
+    while x >= y:
+        up_divisor, divisor = y << 1, y
+        qbit = 1
+        while up_divisor <= x:
+            divisor = up_divisor
+            up_divisor <<= 1
+            qbit <<= 1
+        quotient |= qbit
+        x -= divisor
+    return quotient
 
 
 if __name__ == '__main__':
