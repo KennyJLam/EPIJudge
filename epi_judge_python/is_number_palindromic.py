fed3c1711775d349940cbd3f78f6ba063ee78768
Kenny Lam <lam.kennyj@gmail.com>
2021-07-12 21:42:32 -0400

    Implement check palindromic solution.

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,18 @@
 from test_framework import generic_test
-
+import math
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    if x == 0:
+        return True
+    if x < 0:
+        return False;
+    upper_mask = 10**int(math.log10(x))
+    lower_mask = 1
+    while upper_mask > lower_mask:
+        if (x // upper_mask) % 10 != (x // lower_mask) % 10:
+            return False
+        upper_mask //= 10
+        lower_mask *= 10
     return True
 
 
