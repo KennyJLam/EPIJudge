f40cf457b090e7397d4cb7d52f826c9cc01b815f
Kenny Lam <lam.kennyj@gmail.com>
2021-07-21 23:21:49 -0400

    Implement gcd solution.

--- epi_judge_python/gcd.py
+++ epi_judge_python/gcd.py
@@ -1,9 +1,53 @@
 from test_framework import generic_test
 
 
+# my solution (faster)
+def gcd_a(x: int, y: int) -> int:
+    if x == y:
+        return x
+    a, b = min(x, y), max(x, y)
+    if a == 0:
+        return b
+    if a == 1:
+        return 1
+    while a != b:
+        a, b = min(a, b), max(a,b)
+        grow = a
+        next_grow = a << 1
+        while next_grow < b:
+            grow = next_grow
+            next_grow <<= 1
+        b -= grow
+    return a
+
+
+# book solution
+def gcd_b(x: int, y: int) -> int:
+    if x == y:
+        return x
+    a, b = min(x, y), max(x, y)
+    if a == 0:
+        return b
+    if a == 1:
+        return 1
+    mult = 0
+    while a != b:
+        a, b = min(a, b), max(a,b)
+        if not (a & 1) and not (b & 1):
+            mult += 1
+            a >>= 1
+            b >>= 1
+        elif not a & 1:
+            a >>= 1
+        elif not b & 1:
+            b >>= 1
+        else:
+            b -= a
+    return a << mult
+
+
 def gcd(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    return gcd_b(x, y)
 
 
 if __name__ == '__main__':
