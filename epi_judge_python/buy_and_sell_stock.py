b662fe103487750ea30cbe0623443a0992d11439
Kenny Lam <lam.kennyj@gmail.com>
2021-07-22 00:08:28 -0400

    Implement buy and sell stock solution.

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,13 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    max_profit = 0
+    best_px = prices[0]
+    for i in range(1, len(prices)):
+        price = prices[i]
+        max_profit = max(max_profit, price - best_px)
+        best_px = min(best_px, price)
+    return max_profit
 
 
 if __name__ == '__main__':
