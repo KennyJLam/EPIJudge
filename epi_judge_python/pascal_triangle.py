f9dd81ff05e62b819dda1eceb0eebcfe5b726a24
Kenny Lam <lam.kennyj@gmail.com>
2021-07-23 16:05:03 -0400

    Implement pascal triangle solution.

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -4,8 +4,15 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if n == 0:
+        return []
+    ret = [[1]]
+    for i in range(1, n):
+        ret.append([1])
+        for j in range(1, i):
+            ret[i].append(ret[i - 1][j - 1] + ret[i - 1][j])
+        ret[i].append(1)
+    return ret
 
 
 if __name__ == '__main__':
