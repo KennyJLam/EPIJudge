ad112c12516fd57073fd7a64badbbbef84ba34f2
Kenny Lam <lam.kennyj@gmail.com>
2021-07-22 21:40:21 -0400

    Implement next permutation solution.

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -4,8 +4,25 @@ from test_framework import generic_test
 
 
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    prev = -1
+    maxed = True
+    for i in reversed(range(len(perm))):
+        if perm[i] < prev:
+            maxed = False
+            break
+        prev = perm[i]
+    if maxed:
+        return []
+    pos = perm[i]
+    for j in reversed(range(len(perm))):
+        if pos < perm[j]:
+            break
+    ret = perm[:i]
+    ret.append(perm[j])
+    stub = perm[i + 1:]
+    stub[j - i - 1] = perm[i]
+    ret.extend(reversed(stub))
+    return ret
 
 
 if __name__ == '__main__':
