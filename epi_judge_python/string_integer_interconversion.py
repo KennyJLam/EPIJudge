cc20958eec6ef26e7bb9b7b570a6cdeec69ea1fd
Kenny Lam <lam.kennyj@gmail.com>
2021-07-23 16:30:00 -0400

    Implement string integer interconversion solution.

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,13 +3,29 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    if x == 0:
+        return '0'
+    s = []
+    px = abs(x)
+    while px > 0:
+        s.append(chars[px % 10])
+        px //= 10
+    if x < 0:
+        s.append('-')
+    return ''.join(reversed(s))
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    ret = 0
+    mag = 1
+    start_idx = 1 if s[0] == '-' or s[0] == '+' else 0
+    for c in reversed(s[start_idx:]):
+        ret += (ord(c) - ord('0')) * mag
+        mag *= 10
+    if s[0] == '-':
+        ret *= -1
+    return ret
 
 
 def wrapper(x, s):
