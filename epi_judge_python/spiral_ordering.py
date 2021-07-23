36a8a9c04c5266e3da87294644b6782e88c02105
Kenny Lam <lam.kennyj@gmail.com>
2021-07-22 23:16:03 -0400

    Implement spiral ordering solution.

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -3,9 +3,31 @@ from typing import List
 from test_framework import generic_test
 
 
+def spiral_iter(matrix: List[List[int]], start_r, start_c, m_len) -> List[int]:
+    if m_len == 1:
+        return [matrix[start_r][start_c]]
+    ret = []
+    for i in range(m_len):
+        ret.append(matrix[start_r][start_c + i])
+    for i in range(1, m_len):
+        ret.append(matrix[start_r + i][start_c + m_len - 1])
+    for i in reversed(range(m_len - 1)):
+        ret.append(matrix[start_r + m_len - 1][start_c + i])
+    for i in reversed(range(1, m_len - 1)):
+        ret.append(matrix[start_r + i][start_c])
+    return ret
+
+
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    m_len = len(square_matrix)
+    start_r, start_c = 0, 0
+    ret = []
+    while m_len > 0:
+        ret.extend(spiral_iter(square_matrix, start_r, start_c, m_len))
+        m_len -= 2
+        start_r += 1
+        start_c += 1
+    return ret
 
 
 if __name__ == '__main__':
