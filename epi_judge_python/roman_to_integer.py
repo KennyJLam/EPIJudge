77ab2e619c195f17df4d54a6cfc0d22e7627ec27
Kenny Lam <lam.kennyj@gmail.com>
2021-07-25 16:12:28 -0400

    Implement roman to integer solution.

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    cmap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ret = 0
+    i, i_next = 0, 1
+    while i_next < len(s):
+        c, c_next = s[i], s[i_next]
+        i_num, i_num_next = cmap[c], cmap[c_next]
+        if i_num >= i_num_next:
+            ret += i_num
+            i += 1
+            i_next += 1
+        else:
+            ret += i_num_next - i_num
+            i += 2
+            i_next += 2
+    if i < len(s):
+        ret += cmap[s[i]]
+    return ret
 
 
 if __name__ == '__main__':
