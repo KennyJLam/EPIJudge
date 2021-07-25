fcb14d0b9b34ceaf10f92756e4467121c3960f37
Kenny Lam <lam.kennyj@gmail.com>
2021-07-25 15:56:09 -0400

    Implement look and say solution.

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    cur_array, prev_array = ['1'], []
+    for i in range(1, n):
+        prev_array, cur_array = cur_array, prev_array
+        cur_array.clear()
+        num, prev_num, num_count = prev_array[0], prev_array[0], 1
+        for j in range(1, len(prev_array)):
+            num = prev_array[j]
+            if num != prev_num:
+                cur_array.append(str(num_count))
+                cur_array.append(prev_num)
+                prev_num = num
+                num_count = 1
+            else:
+                num_count += 1
+        cur_array.append(str(num_count))
+        cur_array.append(num)
+    return ''.join(cur_array)
 
 
 if __name__ == '__main__':
