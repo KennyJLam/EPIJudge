ad6d55cc3bcb9109d271031174443ee68f3ad7f7
Kenny Lam <lam.kennyj@gmail.com>
2021-07-26 09:59:14 -0400

    Implement sorted lists merge solution.

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,29 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L1 is None:
+        return L2
+    if L2 is None:
+        return L1
+    if L1.data <= L2.data:
+        head, L1 = L1, L1.next
+    else:
+        head, L2 = L2, L2.next
+    tail = head
+    while L1 is not None or L2 is not None:
+        if L1 is None:
+            tail.next = L2
+            break
+        if L2 is None:
+            tail.next = L1
+            break
+        if L1.data <= L2.data:
+            tail.next, L1 = L1, L1.next
+        else:
+            tail.next, L2 = L2, L2.next
+        tail = tail.next
+
+    return head
 
 
 if __name__ == '__main__':
