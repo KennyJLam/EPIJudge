2518408c86facb7f0fd23619e5eac9460601c89b
Kenny Lam <lam.kennyj@gmail.com>
2021-07-26 00:51:20 -0400

    Implement substring match solution.

--- epi_judge_python/substring_match.py
+++ epi_judge_python/substring_match.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def rabin_karp(t: str, s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    sh = sum([ord(c) for c in s])
+    th = sum([ord(c) for c in t[:len(s)]])
+    for i in range(len(t) - len(s) + 1):
+        if i > 0:
+            th -= ord(t[i - 1])
+            th += ord(t[i + len(s) - 1])
+        if sh == th:
+            if t[i:i + len(s)] == s:
+                return i
+    return -1
 
 
 if __name__ == '__main__':
