692edd6f9445c3a14507bfac7e88870da98fd602
Kenny Lam <lam.kennyj@gmail.com>
2021-07-26 12:31:51 -0400

    Implement reverse sublist solution.

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -6,8 +6,28 @@ from test_framework import generic_test
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if start == 0:
+        return L
+    dhead = ListNode()
+    dhead.next = L
+    idx = 1
+    n_insert = dhead
+    while idx < start:
+        n_insert = n_insert.next
+        if n_insert is None:
+            return L
+        idx += 1
+    n_terminal = n_insert
+    while idx <= finish:
+        n_terminal = n_terminal.next
+        idx += 1
+    n_remove = n_insert.next
+    while n_insert.next != n_terminal:
+        n_swap = n_remove.next
+        n_remove.next = n_swap.next
+        n_swap.next = n_insert.next
+        n_insert.next = n_swap
+    return dhead.next
 
 
 if __name__ == '__main__':
