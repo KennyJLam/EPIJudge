0e77e1fa290f619b5dc12283d59a9ed6c8ef6902
Kenny Lam <lam.kennyj@gmail.com>
2021-07-26 00:10:40 -0400

    Implement run length compression solution.

--- epi_judge_python/run_length_compression.py
+++ epi_judge_python/run_length_compression.py
@@ -3,13 +3,28 @@ from test_framework.test_failure import TestFailure
 
 
 def decoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    ret = []
+    i = 0
+    while i < len(s):
+        j = i
+        while j < len(s) and s[j].isnumeric():
+            j += 1
+        for k in range(int(s[i:j])):
+            ret.append(s[j])
+        i = j + 1
+    return ''.join(ret)
 
 
 def encoding(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    ret = []
+    count = 0
+    for i in range(len(s)):
+        count += 1
+        if i == len(s) - 1 or s[i + 1] != s[i]:
+            ret.append(str(count))
+            ret.append(s[i])
+            count = 0
+    return ''.join(ret)
 
 
 def rle_tester(encoded, decoded):
