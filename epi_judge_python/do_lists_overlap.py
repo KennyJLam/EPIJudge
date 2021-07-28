301d1af7468221b8b9965e3b69c3a3ce126b5ed3
Kenny Lam <lam.kennyj@gmail.com>
2021-07-27 23:35:01 -0400

    Implement do lists overlap solution.

--- epi_judge_python/do_lists_overlap.py
+++ epi_judge_python/do_lists_overlap.py
@@ -8,8 +8,77 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def overlapping_lists(l0: ListNode, l1: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if l0 is None or l1 is None:
+        return None
+    is_cycle0 = True
+    if not l0.next or not l0.next.next:
+        is_cycle0 = False
+    else:
+        slow0, fast0 = l0.next, l0.next.next
+        while slow0 is not fast0:
+            if not fast0.next or not fast0.next.next:
+                is_cycle0 = False
+                break
+            slow0 = slow0.next
+            fast0 = fast0.next.next
+
+    is_cycle1 = True
+    if not l1.next or not l1.next.next:
+         is_cycle1 = False
+    else:
+        slow1, fast1 = l1.next, l1.next.next
+        while slow1 is not fast1:
+            if not fast1.next or not fast1.next.next:
+                is_cycle1 = False
+                break
+            slow1 = slow1.next
+            fast1 = fast1.next.next
+
+    if is_cycle0 and not is_cycle1:
+        return None
+    elif not is_cycle0 and is_cycle1:
+        return None
+    elif is_cycle0 and is_cycle1:
+        fast0 = slow0.next
+        cycle0_len = 1
+        is_intersect = fast0 is slow1
+        while fast0 is not slow0:
+            fast0 = fast0.next
+            cycle0_len += 1
+            if fast0 is slow1:
+                is_intersect = True
+        if not is_intersect:
+            return None
+        slow0, fast0 = l0, l0
+        for _ in range(cycle0_len):
+            fast0 = fast0.next
+        while fast0 is not slow0:
+            slow0 = slow0.next
+            fast0 = fast0.next
+        return slow0
+    else:
+        len0 = 1
+        node = l0
+        while node.next:
+            len0 += 1
+            node = node.next
+        len1 = 1
+        node = l1
+        while node.next:
+            len1 += 1
+            node = node.next
+        diff = abs(len0 - len1)
+        if len0 >= len1:
+            n0, n1 = l0, l1
+        else:
+            n0, n1 = l1, l0
+        for _ in range(diff):
+            n0 = n0.next
+        while n0 is not n1:
+            n0 = n0.next
+            n1 = n1.next
+        return n0
+
 
 
 @enable_executor_hook
