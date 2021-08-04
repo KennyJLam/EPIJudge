7f1cb21a768d8093d3ca32d808e5385557699793
Kenny Lam <lam.kennyj@gmail.com>
2021-08-03 22:28:15 -0400

    Implement even odd list merge solution.

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -5,8 +5,26 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if not L:
+        return None
+    if not L.next:
+        return L
+    ehead, etail = L, L
+    ohead, otail = L.next, L.next
+    cursor = L.next.next
+    bit = 0
+    while cursor:
+        if bit:
+            otail.next = cursor
+            otail = otail.next
+        else:
+            etail.next = cursor
+            etail = etail.next
+        cursor = cursor.next
+        bit ^= 1
+    etail.next = ohead
+    otail.next = None
+    return ehead
 
 
 if __name__ == '__main__':
