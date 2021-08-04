70117fda7c63e834fa17f43758d4edd56029f4ac
Kenny Lam <lam.kennyj@gmail.com>
2021-08-03 23:14:55 -0400

    Implement pivot list solution.

--- epi_judge_python/pivot_list.py
+++ epi_judge_python/pivot_list.py
@@ -8,8 +8,29 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def list_pivoting(l: ListNode, x: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if not l:
+        return None
+    lt_head, eq_head, gt_head = ListNode(), ListNode(), ListNode()
+    lt_tail, eq_tail, gt_tail = lt_head, eq_head, gt_head
+    cursor = l
+    while cursor:
+        if cursor.data < x:
+            lt_tail.next = cursor
+            lt_tail = cursor
+        elif cursor.data == x:
+            eq_tail.next = cursor
+            eq_tail = cursor
+        else:
+            gt_tail.next = cursor
+            gt_tail = cursor
+        cursor = cursor.next
+    ret = lt_head.next if lt_head.next else eq_head.next if eq_head.next else gt_head.next
+    if lt_head.next:
+        lt_tail.next = eq_head.next if eq_head.next else gt_head.next if gt_head.next else None
+    if eq_head.next:
+        eq_tail.next = gt_head.next if gt_head.next else None
+    gt_tail.next = None
+    return ret
 
 
 def linked_to_list(l):
