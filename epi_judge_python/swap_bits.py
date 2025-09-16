b819ff0460547370ce55b52006879cc147a35c49
Kenny Lam <lam.kennyj@gmail.com>
2025-09-15 20:40:46 -0400

    Improve 4.02

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -1,15 +1,21 @@
 from test_framework import generic_test
 
+# Less Efficient
+# def swap_bits(x, i, j):
+#     if i == j:
+#         return x
+#     if i > j:
+#         i, j = j, i
+#     x ^= (x & (1 << i)) << (j - i)
+#     x ^= (x & (1 << j)) >> (j - i)
+#     x ^= (x & (1 << i)) << (j - i)
+#
+#     return x
 
 def swap_bits(x, i, j):
-    if i == j:
+    if ((x >> i) & 1) ^ ((x >> j) & 1) == 0:
         return x
-    if i > j:
-        i, j = j, i
-    x ^= (x & (1 << i)) << (j - i)
-    x ^= (x & (1 << j)) >> (j - i)
-    x ^= (x & (1 << i)) << (j - i)
-
+    x ^= (1 << i) | (1 << j)
     return x
 
 
