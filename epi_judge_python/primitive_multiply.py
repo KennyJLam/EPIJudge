4db102d953e124130b16aeaffbdd4610adfcc5b8
Kenny Lam <lam.kennyj@gmail.com>
2025-09-16 07:48:32 -0400

    Implement 4.05

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
+def add(x: int, y: int) -> int:
+    ret_sum, carry = x, y
+    while carry:
+        ret_sum, carry = ret_sum ^ carry, (ret_sum & carry) << 1
+
+    return ret_sum
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    ret_prod = 0
+    addend, residual = x, y
+    while residual:
+        if residual & 1:
+            ret_prod = add(ret_prod, addend)
+        residual >>= 1
+        addend <<= 1
+    return ret_prod
 
 
 if __name__ == '__main__':
