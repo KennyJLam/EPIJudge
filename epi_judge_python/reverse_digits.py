390c92e18d4b74ad20b5e8d3fdad2486c96fac52
Kenny Lam <lam.kennyj@gmail.com>
2025-09-16 20:12:45 -0400

    Implement 4.08

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    rev_x = 0
+    is_neg = x < 0
+    x = abs(x)
+    while x > 0:
+        rev_x *= 10
+        rev_x += x % 10
+        x //= 10
+    return rev_x * (-1 if is_neg else 1)
 
 
 if __name__ == '__main__':
