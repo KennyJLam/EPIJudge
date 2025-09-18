0115bb66596377811b72449a84f859e6899dbf09
Kenny Lam <lam.kennyj@gmail.com>
2025-09-17 23:15:49 -0400

    Implement 5.02

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,15 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    i = len(A) - 1
+    while i >= 0:
+        A[i] = (A[i] + 1) % 10
+        if A[i] != 0:
+            break
+        i -= 1
+    if A[0] == 0:
+        A.insert(0, 1)
+    return A
 
 
 if __name__ == '__main__':
