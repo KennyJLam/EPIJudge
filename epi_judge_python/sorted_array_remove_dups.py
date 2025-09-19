01c433381da68b0d7af92aa1156f0fea149da86d
Kenny Lam <lam.kennyj@gmail.com>
2025-09-19 10:38:49 -0400

    Implement 5.05

--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -7,8 +7,14 @@ from test_framework.test_utils import enable_executor_hook
 
 # Returns the number of valid entries after deletion.
 def delete_duplicates(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if len(A) == 0:
+        return 0
+    set_end = 0
+    for i in range(1, len(A)):
+        if A[i] != A[set_end]:
+            set_end += 1
+            A[set_end] = A[i]
+    return set_end + 1
 
 
 @enable_executor_hook
