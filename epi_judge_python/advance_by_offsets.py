47757a8369e54af63e780f7b6ece584060a28d13
Kenny Lam <lam.kennyj@gmail.com>
2025-09-19 10:27:44 -0400

    Implement 5.04

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -4,7 +4,13 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
+    max_step = 0
+    for i in range(len(A)):
+        if i > max_step:
+            return False
+        max_step = max(max_step, i + A[i])
+        if max_step >= len(A) - 1:
+            return True
     return True
 
 
