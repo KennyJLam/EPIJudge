27eed66688390c40e4f790baae238f9a2711cc7f
Kenny Lam <lam.kennyj@gmail.com>
2025-09-21 10:48:41 -0400

    Implement 5.09

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -1,12 +1,28 @@
 from typing import List
 
 from test_framework import generic_test
+import math
 
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if n <= 1:
+        return []
+    primes = [2]
+    current = 3
+    while current <= n:
+        cap = int(math.sqrt(current)) + 1
+        for prime in primes[1:]:
+            if prime > cap:
+                primes.append(current)
+                break
+            if current % prime == 0:
+                break
+        else:
+            primes.append(current)
+        current += 2
+
+    return primes
 
 
 if __name__ == '__main__':
