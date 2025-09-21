42c3b0093cf17913dfe325514e7f74ab8044b131
Kenny Lam <lam.kennyj@gmail.com>
2025-09-20 21:11:42 -0400

    Implement 5.07

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -4,8 +4,16 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    if len(prices) < 2:
+        return 0
+    best_buy1 = -prices[0]
+    best_sell1, best_buy2, best_sell2 = -float('inf'), -float('inf'), 0
+    for i in range(1, len(prices)):
+        best_sell2 = max(best_sell2, prices[i] + best_buy2)
+        best_buy2 = max(best_buy2, -prices[i] + best_sell1)
+        best_sell1 = max(best_sell1, prices[i] + best_buy1)
+        best_buy1 = max(best_buy1, -prices[i])
+    return max(0, max(best_sell1, best_sell2))
 
 
 if __name__ == '__main__':
