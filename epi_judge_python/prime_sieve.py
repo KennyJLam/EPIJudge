affc9fbdff0a988e9afa7ec2ed58e1bed59326f4
Kenny Lam <klam@kingstreet.com>
2025-09-22 16:37:05 -0400

    Optimize 5.09

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -5,22 +5,39 @@ import math
 
 
 # Given n, return all primes up to and including n.
+# Trial division method
+# def generate_primes(n: int) -> List[int]:
+#     if n <= 1:
+#         return []
+#     primes = [2]
+#     current = 3
+#     while current <= n:
+#         cap = int(math.sqrt(current)) + 1
+#         for prime in primes[1:]:
+#             if prime > cap:
+#                 primes.append(current)
+#                 break
+#             if current % prime == 0:
+#                 break
+#         else:
+#             primes.append(current)
+#         current += 2
+#
+#     return primes
+
+# Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
     if n <= 1:
         return []
     primes = [2]
-    current = 3
-    while current <= n:
-        cap = int(math.sqrt(current)) + 1
-        for prime in primes[1:]:
-            if prime > cap:
-                primes.append(current)
-                break
-            if current % prime == 0:
-                break
-        else:
-            primes.append(current)
-        current += 2
+    num_candidates = (n - 3) // 2 + 1
+    candidates = [True] * num_candidates
+    for i in range(num_candidates):
+        prime = 2*i + 3
+        if candidates[i]:
+            primes.append(prime)
+        for j in range(2*i**2 + 6*i + 3, num_candidates, prime):
+            candidates[j] = False
 
     return primes
 
