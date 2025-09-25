ef634611a570193f499954622d4b11283a9e5bb0
Kenny Lam <lam.kennyj@gmail.com>
2025-09-24 20:37:12 -0400

    Implement 5.11

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -4,8 +4,20 @@ from test_framework import generic_test
 
 
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if len(perm) <= 1:
+        return []
+    i = len(perm) - 2
+    while i >= 0 and perm[i] >= perm[i + 1]:
+        i -= 1
+    if i < 0:
+        return []
+    j = i
+    while (j + 1) < len(perm) and (perm[j + 1] > perm[i]) :
+        j += 1
+    perm[i], perm[j] = perm[j], perm[i]
+    for k in range((len(perm) - i - 1) // 2):
+        perm[i + k + 1], perm[-(k + 1)] = perm[-(k + 1)], perm[i + k + 1]
+    return perm
 
 
 if __name__ == '__main__':
