4ea24c965106832f2d43f2a28ce7782cf139562b
Kenny Lam <lam.kennyj@gmail.com>
2025-09-28 21:32:40 -0400

    Implement 5.17

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -5,7 +5,26 @@ from test_framework import generic_test
 
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
+    for i in range(9):
+        row_nums = set()
+        col_nums = set()
+        sq_nums = set()
+        for j in range(9):
+            if partial_assignment[i][j] != 0:
+                if partial_assignment[i][j] in row_nums:
+                    return False
+                row_nums.add(partial_assignment[i][j])
+            if partial_assignment[j][i] != 0:
+                if partial_assignment[j][i] in col_nums:
+                    return False
+                col_nums.add(partial_assignment[j][i])
+            i_sq = (i // 3) * 3 + j // 3
+            j_sq = (i % 3) * 3 + j % 3
+            if partial_assignment[i_sq][j_sq] != 0:
+                if partial_assignment[i_sq][j_sq] in sq_nums:
+                    return False
+                sq_nums.add(partial_assignment[i_sq][j_sq])
+
     return True
 
 
