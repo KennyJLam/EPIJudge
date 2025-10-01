82e71f420c674707503fa61678f3435b2aeb27ce
Kenny Lam <klam@kingstreet.com>
2025-10-01 09:10:21 -0400

    Implement 5.20

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -4,8 +4,15 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    out = []
+    for i in range(n):
+        out.append([])
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                out[i].append(1)
+            else:
+                out[i].append(out[i - 1][-j] + out[i - 1][-j - 1])
+    return out
 
 
 if __name__ == '__main__':
